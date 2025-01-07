da5a05be1f726c3477f731d3df434422b0dc4fa6
kawshan <kawshan.virantha9999@gmail.com>
2025-01-07 09:50:24 +0530

    version 01

---
+++ src/main/java/com/aukdevelopment/ytjwttutorial/YtjwttutorialApplication.java
@@ -0,0 +1,14 @@
+package com.aukdevelopment.ytjwttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtjwttutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtjwttutorialApplication.class, args);
+		System.out.println("application started ✔");
+	}
+
+}
---
+++ src/test/java/com/aukdevelopment/ytjwttutorial/YtjwttutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aukdevelopment.ytjwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtjwttutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
